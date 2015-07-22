438e9083377541db0781c32ef8b47da7c3e443ba
Raghu Betina <rbetina@github.com>
2015-07-22 18:44:50 -0500

    Solves paper and scissors

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,4 +12,32 @@ class GameController < ApplicationController
 
     render("rock.html.erb")
   end
+
+  def paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render("paper.html.erb")
+  end
+
+  def scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    elsif @computer_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render("scissors.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/rock", { :controller => "game", :action => "rock" })
+  get("/paper", { :controller => "game", :action => "paper" })
+  get("/scissors", { :controller => "game", :action => "scissors" })
 end
