208f1abeeb660551d984484f31ad1ba6fb2112d7
Raghu Betina <rbetina@github.com>
2015-07-22 20:01:19 -0500

    Switches to one, dynamic RCAV

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,49 +1,78 @@
 class GameController < ApplicationController
-  def rock
-    @user_move = "rock"
+
+  def flexible
+
+    # Rails, behind the scenes:
+    # params = {"move"=>"rock"}
+
+    @user_move = params["move"]
 
     @computer_move = ["rock", "paper", "scissors"].sample
 
-    if @computer_move == "rock"
+    if @user_move == @computer_move
       @outcome = "tied"
-    elsif @computer_move == "paper"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @outcome = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
       @outcome = "lost"
-    elsif @computer_move == "scissors"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @outcome = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @outcome = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
       @outcome = "won"
     end
 
     render("move.html.erb")
   end
 
-  def paper
-    @user_move = "paper"
+  # def rock
+  #   @user_move = "rock"
 
-    @computer_move = ["rock", "paper", "scissors"].sample
+  #   @computer_move = ["rock", "paper", "scissors"].sample
 
-    if @computer_move == "rock"
-      @outcome = "won"
-    elsif @computer_move == "paper"
-      @outcome = "tied"
-    elsif @computer_move == "scissors"
-      @outcome = "lost"
-    end
+  #   if @computer_move == "rock"
+  #     @outcome = "tied"
+  #   elsif @computer_move == "paper"
+  #     @outcome = "lost"
+  #   elsif @computer_move == "scissors"
+  #     @outcome = "won"
+  #   end
 
-    render("move.html.erb")
-  end
+  #   render("move.html.erb")
+  # end
 
-  def scissors
-    @user_move = "scissors"
+  # def paper
+  #   @user_move = "paper"
 
-    @computer_move = ["rock", "paper", "scissors"].sample
+  #   @computer_move = ["rock", "paper", "scissors"].sample
 
-    if @computer_move == "rock"
-      @outcome = "lost"
-    elsif @computer_move == "paper"
-      @outcome = "won"
-    elsif @computer_move == "scissors"
-      @outcome = "tied"
-    end
+  #   if @computer_move == "rock"
+  #     @outcome = "won"
+  #   elsif @computer_move == "paper"
+  #     @outcome = "tied"
+  #   elsif @computer_move == "scissors"
+  #     @outcome = "lost"
+  #   end
 
-    render("move.html.erb")
-  end
+  #   render("move.html.erb")
+  # end
+
+  # def scissors
+  #   @user_move = "scissors"
+
+  #   @computer_move = ["rock", "paper", "scissors"].sample
+
+  #   if @computer_move == "rock"
+  #     @outcome = "lost"
+  #   elsif @computer_move == "paper"
+  #     @outcome = "won"
+  #   elsif @computer_move == "scissors"
+  #     @outcome = "tied"
+  #   end
+
+  #   render("move.html.erb")
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get("/rock", { :controller => "game", :action => "rock" })
-  get("/paper", { :controller => "game", :action => "paper" })
-  get("/scissors", { :controller => "game", :action => "scissors" })
+  # get("/rock", { :controller => "game", :action => "rock" })
+  # get("/paper", { :controller => "game", :action => "paper" })
+  # get("/scissors", { :controller => "game", :action => "scissors" })
+
+  get("/flexible/:move", { :controller => "game", :action => "flexible" })
 end
